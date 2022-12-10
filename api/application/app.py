58eb3810dcce98cbdac5a172799afe1bfdfafa01
Eduardo Frota Vicentini <eduardofvicentini@gmail.com>
2022-12-10 20:23:15 -0300

    Finished function

--- api/application/app.py
+++ api/application/app.py
@@ -9,26 +9,61 @@ from sqlalchemy import insert
 def lambda_handler(event, context):
     # TODO implement
     # Read csv
-    data = get_file_from_s3('csv-storage-ede', 'arquivo_exemplo.csv')
-    
-    fetched_csv = handle_csv(data)
-    
-    db = Dabatase()
-
-    # tabela = db.db.table('cessao_fundo',Dabatase.metadata)
-    Cessao_Fundo.create(db.engine)
+    body = json.loads(event['body'])
+    try:
+        data = get_file_from_s3(body['bucket_name'], body['object_key'])
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': {
+                'message': 'error trying to get file from s3',
+                'error': e.args[0]  
+            }  
+        } 
+        
+    try:    
+        fetched_csv = handle_csv(data)
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': {
+                'message': 'error converting csv data',
+                'error': e.args[0]  
+            }  
+        }
     
     try:
-        db.connection.execute(insert(Cessao_Fundo), fetched_csv['data'])
+        db = Dabatase()
     except Exception as e:
-        print(e)    
-    # here each item will be added in database.    
-
+        return {
+            'statusCode': 500,
+            'body': {
+                'message': 'database connection error',
+                'error': e.args[0]
+            }   
+        }
+    # # tabela = db.db.table('cessao_fundo',Dabatase.metadata)
+    if not db.engine.dialect.has_table(db.connection, Cessao_Fundo.__tablename__):
+        Cessao_Fundo.create(db.engine)
+    
+    try:
+        db.connection.execute(insert(Cessao_Fundo), fetched_csv['data'])  # here each item will be added in database.  
+    except Exception as e:
+        return {
+        'statusCode': 200,
+        'body': {
+            'message': 'error inserting into database',
+            'error': e.args[0]
+        }
+    } 
+    
     return {
         'statusCode': 200,
-        'success': fetched_csv['success'],
-        'error': fetched_csv['error']['count']
-        # 'body': json.dumps(fetched_csv['data'])
+        'body': {
+            'success': fetched_csv['success'],
+            'error': fetched_csv['error']['count'],
+            'error_list': fetched_csv['error']['error_list']
+        }
     }
 
 def get_file_from_s3(ABucket, AKey):
--- api/application/csv_handler.py
+++ api/application/csv_handler.py
@@ -1,4 +1,3 @@
-import csv
 import re
 import datetime
 
@@ -12,41 +11,43 @@ def handle_csv(data):
 
     # fetch into a array
     result = []
-    error_list = []    
+    error_list = []
     for line in fetched_lines:
         try:
             result.append(create_cessao_fundo_object(line))
         except Exception as e:
-            error_list.append(e)
+            error_list.append({'originador': line[0], 'ccb': line[4], 'error':e.args[0]})
 
-    fetched_object = { 'success': len(result), 'error': {'count':len(error_list), 'error_list': error_list}, 'data': result }        
+    fetched_object = { 'success': len(result), 'error': {'count':len(error_list), 'error_list': error_list}, 'data': result }     
     return fetched_object
    
-def create_cessao_fundo_object(ALine):
-    # print(ALine)
+def create_cessao_fundo_object(ACSVLine):
+    # print(ACSVLine)
     # format
     format = '%Y-%m-%d'
-    
-    object = {
-        'ORIGINADOR'          :   ALine[0], # EMPIRICA INVESTIMENTOS
-        'DOC_ORIGINADOR'      :   re.sub(r'[.|/|-]','',ALine[1]), # 10.896.871/0001-99 -> 10896871000199
-        'CEDENTE'             :   ALine[2], # EMPIRICA HOLDING S.A.
-        'DOC_CEDENTE'         :   ALine[3], # 42351640000196
-        'CCB'                 :   ALine[4], # 379654
-        'ID_EXTERNO'          :   ALine[5], # 12359
-        'CLIENTE'             :   ALine[6], # Pessoa exemplo 1
-        'CPF_CNPJ'            :   re.sub(r'[.|/|-]','',ALine[7]), # 12.345.678/0001-10 -> 12345678000110
-        'ENDERECO'            :   ALine[8], # R RODOVIA ADMAR GONZAGA 125
-        'CEP'                 :   ALine[9], # 60752040
-        'CIDADE'              :   ALine[10], # FLORIANOPOLIS
-        'UF'                  :   ALine[11], # SC
-        'VALOR_DO_EMPRESTIMO' :   float(re.sub(r'[,]','.', ALine[12])), # 127,86
-        'VALOR_PARCELA'       :   float(re.sub(r'[,]','.', ALine[14])), # 17,87
-        'TOTAL_PARCELAS'      :   ALine[19], # 8
-        'PARCELA'             :   ALine[20], # 1
-        'DATA_DE_EMISSAO'     :   datetime.datetime.strptime(ALine[23], '%d/%m/%Y').strftime('%Y-%m-%d'), # 10/05/2022
-        'DATA_DE_VENCIMENTO'  :   datetime.datetime.strptime(ALine[24], '%d/%m/%Y').strftime('%Y-%m-%d'), # 14/07/2022
-        'PRECO_DE_AQUISICAO'  :   float(re.sub(r'[,]','.', ALine[26])) # 15.76
-    }
+    try:
+        object = {
+            'ORIGINADOR'          :   ACSVLine[0], # EMPIRICA INVESTIMENTOS
+            'DOC_ORIGINADOR'      :   re.sub(r'[.|/|-]','',ACSVLine[1]), # 10.896.871/0001-99 -> 10896871000199
+            'CEDENTE'             :   ACSVLine[2], # EMPIRICA HOLDING S.A.
+            'DOC_CEDENTE'         :   ACSVLine[3], # 42351640000196
+            'CCB'                 :   ACSVLine[4], # 379654
+            'ID_EXTERNO'          :   ACSVLine[5], # 12359
+            'CLIENTE'             :   ACSVLine[6], # Pessoa exemplo 1
+            'CPF_CNPJ'            :   re.sub(r'[.|/|-]','',ACSVLine[7]), # 12.345.678/0001-10 -> 12345678000110
+            'ENDERECO'            :   ACSVLine[8], # R RODOVIA ADMAR GONZAGA 125
+            'CEP'                 :   ACSVLine[9], # 60752040
+            'CIDADE'              :   ACSVLine[10], # FLORIANOPOLIS
+            'UF'                  :   ACSVLine[11], # SC
+            'VALOR_DO_EMPRESTIMO' :   float(re.sub(r'[,]','.', ACSVLine[12])), # 127,86
+            'VALOR_PARCELA'       :   float(re.sub(r'[,]','.', ACSVLine[14])), # 17,87
+            'TOTAL_PARCELAS'      :   ACSVLine[19], # 8
+            'PARCELA'             :   ACSVLine[20], # 1
+            'DATA_DE_EMISSAO'     :   datetime.datetime.strptime(ACSVLine[23], '%d/%m/%Y').strftime('%Y-%m-%d'), # 10/05/2022
+            'DATA_DE_VENCIMENTO'  :   datetime.datetime.strptime(ACSVLine[24], '%d/%m/%Y').strftime('%Y-%m-%d'), # 14/07/2022
+            'PRECO_DE_AQUISICAO'  :   float(re.sub(r'[,]','.', ACSVLine[26])) # 15.76
+        }
+    except Exception as Err:
+        raise Err
 
     return object
